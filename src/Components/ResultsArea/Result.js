cf366b7380299cd38748bf7405c812892ea37c54
Uzair Shamim <uzashamim@gmail.com>
2019-03-28 14:06:40 -0400

    Added storybook

--- src/Components/ResultsArea/Result.js
+++ src/Components/ResultsArea/Result.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
+import Chip from "@material-ui/core/Chip";
+import Paper from "@material-ui/core/Paper";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Link } from "@material-ui/icons";
+
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 
 class Result extends Component {
   constructor(props) {
@@ -36,59 +39,70 @@ class Result extends Component {
   }
 
   render() {
+    console.log("Diet: ", this.props.result.categories);
     return (
-      <Card
-        style={{
-          width: 300
-        }}
-      >
-        <CardHeader
+      // <Card
+      //   style={{
+      //     width: 300
+      //   }}
+      // >
+      //   <CardMedia
+      //     image={this.props.result.image}
+      //     title={this.props.result.label}
+      //     style={{ height: 0, paddingTop: "56.25%" }}
+      //   />
+      //   <CardContent>
+      //     <Typography gutterBottom variant="h5" component="h2" noWrap>
+      //       {this.props.result.label}
+      //     </Typography>
+      //     <div
+      //       style={{
+      //         height: "40px",
+      //         width: "100%",
+      //         whiteSpace: "nowrap",
+      //         overflowX: "scroll"
+      //       }}
+      //     >
+      //       {this.props.result.categories.map(category => {
+      //         let icon = null;
+      //         return <Chip icon={icon} label={category} />;
+      //       })}
+      //     </div>
+      //   </CardContent>
+      //   <CardActions disableActionSpacing>
+      //     <IconButton
+      //       onClick={this.handleExpandClick}
+      //       aria-expanded={this.state.expanded}
+      //       aria-label="Show more"
+      //     >
+      //       <ExpandMoreIcon />
+      //     </IconButton>
+      //     <IconButton onClick={this.handleLinkClick} aria-label="Go to website">
+      //       <Link />
+      //     </IconButton>
+      //   </CardActions>
+      //   {/* <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+      //     <CardContent>
+      //       <Typography paragraph>Method:</Typography>
+      //       <Typography>
+      //         Heat 1/2 cup of the broth in a pot until simmering, add saffron
+      //         and set aside for 10 minutes.
+      //       </Typography>
+      //     </CardContent>
+      //   </Collapse> */}
+      // </Card>
+
+      <GridListTile key={this.props.result.image} cols={1}>
+        <img src={this.props.result.image} alt={this.props.result.label} />
+        {/* <GridListTileBar
           title={this.props.result.label}
-          subheader={"Servings: " + this.props.result.yield}
-          titleTypographyProps={{
-            noWrap: true
-          }}
-        />
-        <CardMedia
-          image={this.props.result.image}
-          title={this.props.result.label}
-          style={{ height: 0, paddingTop: "56.25%" }}
-        />
-        {/* <CardContent>
-          <Typography component="ul">
-            {this.props.result.ingredientLines.map((ingredient, index) => {
-              if (index <= 4 || this.state.expanded) {
-                return (
-                  <Typography component="li">
-                    <Typography component="p">{ingredient}</Typography>
-                  </Typography>
-                );
-              }
-            })}
-          </Typography>
-        </CardContent> */}
-        <CardActions disableActionSpacing>
-          <IconButton
-            onClick={this.handleExpandClick}
-            aria-expanded={this.state.expanded}
-            aria-label="Show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-          <IconButton onClick={this.handleLinkClick} aria-label="Go to website">
-            <Link />
-          </IconButton>
-        </CardActions>
-        {/* <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron
-              and set aside for 10 minutes.
-            </Typography>
-          </CardContent>
-        </Collapse> */}
-      </Card>
+          // actionIcon={
+          //   <IconButton>
+          //     <StarBorderIcon className={classes.title} />
+          //   </IconButton>
+          // }
+        /> */}
+      </GridListTile>
     );
   }
 }
--- src/Components/ResultsArea/ResultsArea.js
+++ src/Components/ResultsArea/ResultsArea.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Result from "./Result";
 import Settings from "../Settings/Settings";
+import GridList from "@material-ui/core/GridList";
 
 class ResultsArea extends Component {
   render() {
@@ -8,26 +9,29 @@ class ResultsArea extends Component {
     return (
       <div className="Results-Wrapper">
         <Settings className="settings" />
-        <div className="Results-Area">
+        {/* <div className="Results-Area">
           <ul
             style={{
               listStyleType: "none",
               listStyleImage: "none"
             }}
-          >
-            {this.props.results.length > 0 &&
-              this.props.results.map(result => (
-                <li style={{ float: "left", margin: "20px" }}>
-                  <Result result={result} />
-                </li>
-              ))}
-            {this.props.results.length === 0 && (
-              <b>
-                No results found. Please try different filters or search term.
-              </b>
-            )}
-          </ul>
-        </div>
+          > */}
+        <GridList cellHeight={160} cellWidth={100}>
+          {this.props.results.length > 0 &&
+            this.props.results.map(result => (
+              // <li style={{ float: "left", margin: "20px" }}>
+              //   <Result result={result} />
+              // </li>
+              <Result result={result} />
+            ))}
+          {this.props.results.length === 0 && (
+            <b>
+              No results found. Please try different filters or search term.
+            </b>
+          )}
+        </GridList>
+        {/* </ul>
+        </div> */}
       </div>
     );
   }
